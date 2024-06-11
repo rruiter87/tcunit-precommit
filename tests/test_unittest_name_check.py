0580c1a3e26c2a8d9133c4d2c942a0ea43d444a9
Roald Ruiter <roald.ruiter@inspire.ch>
2024-06-11 17:08:16 +0200

    positive check if all names match

--- tests/test_unittest_name_check.py
+++ tests/test_unittest_name_check.py
@@ -15,5 +15,18 @@ def one_failing_one_ok_name():
     )
 
 
+@pytest.fixture()
+def all_matching_names():
+    return (
+        files(tests.data)
+        .joinpath("UnitTestOnlyMatchingNames.TcPOU")
+        .read_text(encoding="UTF-8")
+    )
+
+
 def test_file_with_non_matching_names(one_failing_one_ok_name):
     assert not check_unit_test_method_names(one_failing_one_ok_name)
+
+
+def test_file_with_only_matching_names(all_matching_names):
+    assert check_unit_test_method_names(all_matching_names)
